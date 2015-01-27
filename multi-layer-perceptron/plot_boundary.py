e099dd9b21022ac332c87752ac7fdcbb3f95d3c3
Rémi Domingues <remidomingues@live.fr>
2015-01-27 18:44:20 +0100

    Minor update to plot_boundary

--- multi-layer-perceptron/plot_boundary.py
+++ multi-layer-perceptron/plot_boundary.py
@@ -1,7 +1,7 @@
-import matplotlib.pyplot as plt
+import pylab
 import numpy as np
 
-def plot():
+def plot(perceptron):
     xrange = numpy.arange(−4, 4, 0.1)
     yrange = numpy.arange(−4, 4, 0.1)
     xgrid, ygrid = numpy.meshgrid(xrange, yrange)
@@ -9,15 +9,15 @@ def plot():
     npoints = xgrid.shape[0]∗xgrid.shape[1]
     xcoords = xgrid.reshape((npoints, 1))
     ycoords = ygrid.reshape((npoints, 1))
-    samples = numpy.concatenate((xcoords, ycoords), axis = 1)
+    samples = np.concatenate((xcoords, ycoords), axis=1)
 
-    ones = −numpy.ones(xcoords.shape)
-    samples = numpy.concatenate((samples, ones), axis = 1)
+    ones = np.ones(xcoords.shape)
+    samples = np.concatenate((samples, ones), axis=1)
 
-    indicator = p.mlpfwd(samples)
+    indicator = perceptron.mlpfwd(samples)
     indicator = indicator.reshape(xgrid.shape)
 
-    pylab.contour(xrange, yrange, indicator, (0.5, ))
+    pylab.contour(xrange, yrange, indicator, (0.5,))
     pylab.show()
 
 if __name__ == '__main__':
