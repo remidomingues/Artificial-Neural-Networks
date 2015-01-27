0f5ac7ad3638de79d4340b52c364dc1ed6ce0d41
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2015-01-27 19:07:03 +0100

    Removed trailing spaces

--- multi-layer-perceptron/mlp.py
+++ multi-layer-perceptron/mlp.py
@@ -12,7 +12,7 @@ from numpy import *
 
 class mlp:
     """ A Multi-Layer Perceptron"""
-    
+
     def __init__(self,inputs,targets,nhidden,beta=1,momentum=0.9,outtype='logistic'):
         """ Constructor """
         # Set up network size
@@ -24,19 +24,19 @@ class mlp:
         self.beta = beta
         self.momentum = momentum
         self.outtype = outtype
-    
+
         # Initialise network
         self.weights1 = (random.rand(self.nin+1,self.nhidden)-0.5)*2/sqrt(self.nin)
         self.weights2 = (random.rand(self.nhidden+1,self.nout)-0.5)*2/sqrt(self.nhidden)
 
     def earlystopping(self,inputs,targets,valid,validtargets,eta,niterations=100):
-    
+
         valid = concatenate((valid,-ones((shape(valid)[0],1))),axis=1)
-        
+
         old_val_error1 = 100002
         old_val_error2 = 100001
         new_val_error = 100000
-        
+
         count = 0
         while (((old_val_error1 - new_val_error) > 0.001) or ((old_val_error2 - old_val_error1)>0.001)):
             count+=1
@@ -46,26 +46,26 @@ class mlp:
             old_val_error1 = new_val_error
             validout = self.mlpfwd(valid)
             new_val_error = 0.5*sum((validtargets-validout)**2)
-            
+
         print "Stopped", new_val_error,old_val_error1, old_val_error2
         return new_val_error
-    	
+
     def mlptrain(self,inputs,targets,eta,niterations):
-        """ Train the thing """    
+        """ Train the thing """
         # Add the inputs that match the bias node
         inputs = concatenate((inputs,-ones((self.ndata,1))),axis=1)
         change = range(self.ndata)
-    
+
         updatew1 = zeros((shape(self.weights1)))
         updatew2 = zeros((shape(self.weights2)))
-                      
+
         for n in range(niterations):
-    
+
             self.outputs = self.mlpfwd(inputs)
 
             error = 0.5*sum((targets-self.outputs)**2)
             if (mod(n,100)==0):
-                print "Iteration: ",n, " Error: ",error    
+                print "Iteration: ",n, " Error: ",error
 
             # Different types of output neurons
             if self.outtype == 'linear':
@@ -77,19 +77,19 @@ class mlp:
                 deltao = (targets-self.outputs)/self.ndata
             else:
             	print "error"
-            
+
             deltah = self.hidden*(1.0-self.hidden)*(dot(deltao,transpose(self.weights2)))
 
             updatew1 = eta*(dot(transpose(inputs),deltah[:,:-1])) + self.momentum*updatew1
             updatew2 = eta*(dot(transpose(self.hidden),deltao)) + self.momentum*updatew2
             self.weights1 += updatew1
             self.weights2 += updatew2
-                
+
             # Randomise order of inputs
             random.shuffle(change)
             inputs = inputs[change,:]
             targets = targets[change,:]
-            
+
     def mlpfwd(self,inputs):
         """ Run the network forward """
 
@@ -116,7 +116,7 @@ class mlp:
         # Add the inputs that match the bias node
         inputs = concatenate((inputs,-ones((shape(inputs)[0],1))),axis=1)
         outputs = self.mlpfwd(inputs)
-        
+
         nclasses = shape(targets)[1]
 
         if nclasses==1:
