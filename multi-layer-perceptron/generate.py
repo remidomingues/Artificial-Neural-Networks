c52546537f8926fa2519c5692be5fa2333325120
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2015-01-27 19:13:59 +0100

    First working code, but gives weird results

--- multi-layer-perceptron/generate.py
+++ multi-layer-perceptron/generate.py
@@ -9,9 +9,11 @@ def generate_points(number, mean, std):
 def plot_points(points, style='.'):
     plt.plot(points[:,0], points[:,1], style)
 
-def plot_boundary(perceptron):
-    xrange = np.arange(-4, 4, 0.1)
-    yrange = np.arange(-4, 4, 0.1)
+def plot_boundary(perceptron, inputs):
+    min_x, max_x = min(inputs[:, 0]), max(inputs[:, 0])
+    min_y, max_y = min(inputs[:, 1]), max(inputs[:, 1])
+    xrange = np.arange(min_x - 2, max_x + 2, 0.1)
+    yrange = np.arange(min_y - 2, max_y + 2, 0.1)
     xgrid, ygrid = np.meshgrid(xrange, yrange)
 
     npoints = xgrid.shape[0] * xgrid.shape[1]
@@ -29,14 +31,10 @@ def plot_boundary(perceptron):
 
 if __name__ == '__main__':
     # Generating the points
-    class_a = generate_points(10, mean=(10, 10), std=4)
-    class_b = generate_points(10, mean=(-10, -10), std=4)
-
-    # Plotting the points
-    plt.figure()
-    plot_points(class_a)
-    plot_points(class_b)
-    #plt.show()
+    m_a, m_b = 0, 8
+    std_a, std_b = 1., 1.
+    class_a = generate_points(10, mean=(m_a, m_a), std=std_a)
+    class_b = generate_points(10, mean=(m_b, m_b), std=std_b)
 
     # Generating the concatenated matrix
     class_a = np.hstack([class_a, np.zeros((class_a.shape[0], 1))])
@@ -44,11 +42,16 @@ if __name__ == '__main__':
     data = np.vstack([class_a, class_b])
 
     # MLP training
-    inputs, target = data[:, 2:], np.reshape(data[:, 2], (data.shape[0], 1))
-    perceptron = mlp(inputs, target, nhidden=3)
+    inputs, target = data[:, :2], data[:, 2:]
+    perceptron = mlp(inputs, target, nhidden=100)
 
-    perceptron.mlptrain(inputs, target, eta=0.5, niterations=100)
+    perceptron.mlptrain(inputs, target, eta=0.5, niterations=10000)
 
-    plot_boundary(perceptron)
+
+    # Plotting
+    plt.figure()
+    plot_points(class_a)
+    plot_points(class_b)
+    plot_boundary(perceptron, inputs)
 
     plt.show()
