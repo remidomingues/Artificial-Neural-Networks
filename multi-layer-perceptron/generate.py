b74e8d750118c5a443bc09d0ea890736c9ec8d26
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2015-01-28 12:39:49 +0100

    Added better plotting, "fixed" bug, etc.

--- multi-layer-perceptron/generate.py
+++ multi-layer-perceptron/generate.py
@@ -4,12 +4,14 @@ import numpy as np
 from mlp import mlp
 
 def generate_points(number, mean, std):
-    return mean + np.random.randn(number, 2) * std
+    res = np.random.randn(number, 2)
+    res = np.array(map(lambda xy:( abs(xy[0]) + mean[0], abs(xy[1]) + mean[1]), res))
+    return res
 
-def plot_points(points, style='.'):
-    plt.plot(points[:,0], points[:,1], style)
+def plot_points(points, color, style='.'):
+    plt.plot(points[:,0], points[:,1], style, color=color)
 
-def plot_boundary(perceptron, inputs):
+def plot_boundary(perceptron, inputs, color='#f1312d', linewidth=1):
     min_x, max_x = min(inputs[:, 0]), max(inputs[:, 0])
     min_y, max_y = min(inputs[:, 1]), max(inputs[:, 1])
     xrange = np.arange(min_x - 2, max_x + 2, 0.1)
@@ -21,21 +23,22 @@ def plot_boundary(perceptron, inputs):
     ycoords = ygrid.reshape((npoints, 1))
     samples = np.concatenate((xcoords, ycoords), axis=1)
 
-    ones = np.ones(xcoords.shape)
+    ones = -np.ones(xcoords.shape)
     samples = np.concatenate((samples, ones), axis=1)
 
     indicator = perceptron.mlpfwd(samples)
     indicator = indicator.reshape(xgrid.shape)
 
-    plt.contour(xrange, yrange, indicator, (0.5,))
+    plt.contour(xrange, yrange, indicator, (0.5,), colors=color, linewidths=linewidth)
 
 if __name__ == '__main__':
     # Generating the points
-    m_a, m_b = 0, 8
-    std_a, std_b = 1., 1.
-    n_a, n_b = 50, 50
-    class_a = generate_points(n_a, mean=(m_a, m_a), std=std_a)
-    class_b = generate_points(n_b, mean=(m_b, m_b), std=std_b)
+    m_a, m_b = 0, 1
+    std_a, std_b = 1., .5
+    n_a, n_b = 100, 100
+
+    class_a = (m_a, m_a) + np.random.randn(n_a, 2) * std_a
+    class_b = m_b + np.random.randn(n_b, 2) * std_b
 
     # Generating the concatenated matrix
     class_a = np.hstack([class_a, np.zeros((class_a.shape[0], 1))])
@@ -44,14 +47,17 @@ if __name__ == '__main__':
 
     # MLP training
     inputs, target = data[:, :2], data[:, 2:]
-    perceptron = mlp(inputs, target, nhidden=100)
-
-    perceptron.mlptrain(inputs, target, eta=0.5, niterations=10000)
+    perceptron = mlp(inputs, target, nhidden=3)
 
     # Plotting
     plt.figure()
-    plot_points(class_a)
-    plot_points(class_b)
-    plot_boundary(perceptron, inputs)
+    plot_points(class_a, color='blue')
+    plot_points(class_b, color='red')
+    N_ITERATIONS = 10000
+    STEP = 40
+    for i in xrange(STEP):
+        perceptron.mlptrain(inputs, target, eta=0.01, niterations=N_ITERATIONS/STEP)
+        plot_boundary(perceptron, inputs)
 
+    plot_boundary(perceptron, inputs, color='green', linewidth=4)
     plt.show()
