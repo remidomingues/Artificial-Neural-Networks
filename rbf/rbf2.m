0f362fc587a2c8e2c69b277257aaee0de838c9e6
Rémi Domingues <remidomingues@live.fr>
2015-02-10 20:04:14 +0100

    Added showcases for RBF using EM and CL algorithms

--- rbf/rbf2.m
+++ rbf/rbf2.m
@@ -1,5 +1,5 @@
 x = (0:0.1:2*pi)';
-units = 15;
+units = 62;
 f = square(2 * x);
 
 makerbf;
---
+++ rbf/rbf5_2.m
@@ -0,0 +1,7 @@
+plotinit
+data=read('cluster');
+units=5;
+% Plot data and place rbf units randomly
+vqinit;
+singlewinner=0;
+vqiter; %Successive iterations
