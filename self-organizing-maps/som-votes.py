a606543bd75df001b613e36794d02b06559b4488
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2015-02-16 10:49:27 +0100

    Some cleaning and added a different plot

--- self-organizing-maps/som-votes.py
+++ self-organizing-maps/som-votes.py
@@ -1,18 +1,16 @@
 import house
 
-import math
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.pylab as pylab
 
 def find_winner(input_vec, weights):
     # Difference between x and all prototype vectors
     diff = input_vec - weights
     # Calculate sum of squared differences
     # for all prototypes simultaneously
-    dist = numpy.sum(diff*diff , axis=1)
+    dist = np.sum(diff*diff , axis=1)
     # Locate the winner (index to smallest distance)
-    winner = numpy.argmin(dist)
+    winner = np.argmin(dist)
     return winner, diff
 
 def toGrid(idx):
@@ -23,11 +21,11 @@ def toArray(i, j):
     return i + j * grid_width
 
 if __name__ == '__main__':
-    iterations = 10
+    iterations = 2
     input_size = len(house.votes.values()[0])
-    grid_width = 20 # Grid size = grid_width^2
+    grid_width = 25 # Grid size = grid_width^2
 
-    weights = numpy.random.rand(grid_width*grid_width, input_size)
+    weights = np.random.rand(grid_width*grid_width, input_size)
     for nbh in xrange(grid_width/2-1, 0, -1): # Neighborhood sizes
         for k in xrange(iterations):
             for votes in house.votes.values():
@@ -44,18 +42,34 @@ if __name__ == '__main__':
 
     # Socialistiska (left) = S, V, MP
     # Borgerliga (right) = M, C, FP, KD
-    winner_per_voter = [pylab.zeros((grid_width,grid_width)),
-                        pylab.zeros((grid_width,grid_width))]
-    party_value = {'X': 0, 'R': 7, 'D': 10}
+    winner_per_party = [np.zeros((grid_width, grid_width)),
+                        np.zeros((grid_width, grid_width))]
 
+    party_value = {'X': 0, 'R': 7, 'D': 10}
+    winner_per_voter = dict()
+    winners_list = {'X': [], 'R': [], 'D': []}
     for voter, votes in house.votes.iteritems():
         winner = find_winner(votes, weights)[0]
-        if(house.house[voter][1] is 'R'):
-            winner_per_voter[0][toGrid(winner)] = party_value[house.house[voter][1]]
-        elif(house.house[voter][1] is 'D'):
-            winner_per_voter[1][toGrid(winner)] = party_value[house.house[voter][1]]
+        party = house.house[voter][1]
+
+        # Populating winner_per_voter and winners_list
+        winner_per_voter[voter] = toGrid(winner)
+        winners_list[party].append(toGrid(winner))
+
+        # Populating the matrix
+        if party == 'R':
+            winner_per_party[0][toGrid(winner)] = party_value[party]
+        elif party == 'D':
+            winner_per_party[1][toGrid(winner)] = party_value[party]
 
     # Plot
-    pylab.matshow(winner_per_voter[0])
-    pylab.matshow(winner_per_voter[1])
-    pylab.show()
+    winners_list = {k : np.array(v) for k, v in winners_list.iteritems()}
+
+    size = 16
+    for (party, color) in [('X', 'y'), ('R', 'r'), ('D', 'b')]:
+        plt.plot(winners_list[party][:, 0], winners_list[party][:, 1], '{}o'.format(color), markersize=size)
+        size /= 2
+
+    plt.matshow(winner_per_party[0])
+    plt.matshow(winner_per_party[1])
+    plt.show()
