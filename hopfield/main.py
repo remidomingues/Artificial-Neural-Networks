a0907ee0b7146535d66320593292a83223b842da
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2015-02-25 18:46:12 +0100

    Started the "recovering images" part

--- hopfield/main.py
+++ hopfield/main.py
@@ -21,17 +21,44 @@ def small_patterns():
     # Testing that the patterns are "fixpoints"
     for pattern in patterns:
         updated_pattern = utils.update(weights, pattern)
-        assert utils.samePattern(pattern, updated_pattern)
+        assert utils.samePattern(pattern, updated_pattern), "Simple patterns are not fixpoints"
 
     # Test if the network will recall stored patterns from distorted versions
     for pattern in patterns:
         distorted_pattern = utils.flipper(pattern, len(pattern) / 2)
-        for i in xrange(5):
+        print pattern, distorted_pattern
+        for i in xrange(2):
             distorted_pattern = utils.update(weights, distorted_pattern)
         print pattern, distorted_pattern
         assert utils.samePattern(pattern, distorted_pattern)
 
     print "'Small patterns' experiment succesfull!"
 
+def restoring_images():
+    patterns = [figs.p1, figs.p2, figs.p3]
+    weights = utils.learn(patterns)
+
+    print "! Pattern recovery"
+    for i, (original, noisy) in enumerate([(figs.p1, figs.p11), (figs.p2, figs.p22)]):
+        recovered_pattern = utils.update(weights, noisy)
+
+        if utils.samePattern(recovered_pattern, original):
+            print "  . Correctly recovered pattern {}".format(i+1)
+        else:
+            print "  . Couldn't recover pattern {}".format(i+1)
+
+    sequential_hopfield(weights, figs.p22, figs.p2)
+
+def sequential_hopfield(weights, noisy, original):
+    for i in xrange(40):
+        for _ in xrange(100):
+            utils.updateOne(weights, noisy)
+        show_pattern(noisy)
+        if utils.samePattern(noisy, original):
+            print "Recovered the expected pattern."
+            break
+
 if __name__ == '__main__':
-    small_patterns()
+    #small_patterns()
+    #restoring_images()
+    pass
